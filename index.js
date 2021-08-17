fabfb0c31591ab981c5818a2764086eba265b5da
Mohibur Rahman <mohiburrahman03@gmail.com>
2021-08-17 12:36:37 +0600

    My first Project with JS

---
+++ index.js
@@ -0,0 +1,46 @@
+// Bismillahir Rahmanir Rahim
+// this is my first javascript code
+
+/// for validation
+function validate(username){
+    return (username != null && username != "");
+}
+/// input username 
+function userInput(){
+    while(true)
+    {
+        let userName = prompt('Enter your username');
+        if(validate(userName))
+        {
+            return userName;
+        }
+    }
+}
+// input date of birth
+function userDOB(){
+    while(true)
+    {
+        let DOB = prompt('Enter your Date of Birth in this format YYYY-MM-DD');
+        if(validate(DOB)){
+            return DOB;
+        }
+    }
+}
+// Age calculation 
+function calculateAge(userName , DOB){
+    let birthDate = new Date(DOB);
+    let currentDate = new Date(Date.now());
+    let ageInMiliSecond = currentDate - birthDate.getTime();
+    let ageInDateFormat = new Date(ageInMiliSecond);
+    let seconds = ageInDateFormat.getSeconds();
+    let minutes = ageInDateFormat.getMinutes();
+    let hours = ageInDateFormat.getHours();
+    let months = ageInDateFormat.getMonth();
+    let years = ageInDateFormat.getFullYear() - 1970;
+    let message = "Hello " + userName  + " Your Age is " + years + " Years " + 
+    months + " Months " + hours + " Hours " + minutes + " Minutes " + seconds + " Seconds";
+    document.getElementById("demo").innerHTML = message;
+}
+let userName = userInput();
+let DOB = userDOB();
+setInterval(function(){calculateAge(userName, DOB)}, 1000);
